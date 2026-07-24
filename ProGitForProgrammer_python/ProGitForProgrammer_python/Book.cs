b8fd0efa38bcf9e92bea328cf70119f7a4f4f3b2
YaoDe <huangyaode94@gmail.com>
2026-07-24 15:13:29 +0800

    add property

--- ProGitForProgrammer_python/ProGitForProgrammer_python/Book.cs
+++ ProGitForProgrammer_python/ProGitForProgrammer_python/Book.cs
@@ -5,7 +5,9 @@ namespace ProGitForProgrammers
     class Book
     {
         public string Title{ get; set; }
+
         public List<string> Authors { get; set; }
+
         public DateTime PublicationDate { get; set; } 
     }
 }
