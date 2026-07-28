b25c5cb1d4f45c84b8eb2a76b2c0768b3ac49629
YaoDe <huangyaode94@gmail.com>
2026-07-28 17:12:11 +0800

    newfeature branch

--- ProGitForProgrammer_python/ProGitForProgrammer_python/ProGitForProgrammer_python.py
+++ ProGitForProgrammer_python/ProGitForProgrammer_python/ProGitForProgrammer_python.py
@@ -3,10 +3,6 @@ name=["Brian","wendy","Nicole"]
 for nm in name:
     print(nm)
 
-<<<<<<< HEAD
-print ("a line add to show staging area")
-
-print("å·)
-=======
 print("this line add to show the staging area")
->>>>>>> b8fd0efa38bcf9e92bea328cf70119f7a4f4f3b2
+
+print("newfeature")
